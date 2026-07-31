78b9b39a8d499c0bfffaae91abdd7577ce9d55ab
Dhavaselvan <dhavaselvank.aids2024@citchennai.net>
2026-07-31 11:23:09 +0530

    Delete exp1.py

--- exp1.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-sentiment_analysis = pipeline("sentiment-analysis")
-
-text = "The Generative AI workshop was extremely informative and useful."
-
-result = sentiment_analyzer(text)
-
-print(result)
